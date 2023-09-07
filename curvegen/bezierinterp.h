07d2a26f8b6ba7b04679e30aea09ca91bd1d77eb
Unimprezzed <unimprezzed@gmail.com>
2023-09-06 21:30:33 -0500

    Wrote an efficient function for pre-computing binomial coefficients. Not sure it works or not.

--- curvegen/bezierinterp.h
+++ curvegen/bezierinterp.h
@@ -2,5 +2,6 @@
 #include "interpmethod.h"
 template <typename T> 
 class BezierCubicInterpolation : public InterpolationMethod<T>{
-    
-}
+    public: 
+        T evaluate(std::vector<T> P, float t);
+};
--- curvegen/hermiteinterp.h
+++ curvegen/hermiteinterp.h
@@ -2,5 +2,6 @@
 #include "interpmethod.h"
 template <typename T> 
 class HermiteCubicInterpolation : public InterpolationMethod<T>{
-    
-}
+    public: 
+        T evaluate(std::vector<T> P, float t);
+};
